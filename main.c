04186925c4efccf951986124459d9eb884f91de3
Szymon Urbaś <szymon.urbas@aol.com>
2017-02-15 21:56:15 +0100

    Add a dummy main.c file with a dummy main function

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ *
+ * main.c
+ *
+ * Created at:  15 Feb 2017 21:49:15 +0100 (CET)
+ *
+ * Author:  Szymon Urbaś <szymon.urbas@aol.com>
+ *
+ * License:  please visit the LICENSE file for details.
+ *
+ */
+
+int main(void)
+{
+  while (1);
+}
+
+/*
+ * vi: ft=c:ts=2:sw=2:expandtab
+ */
+
