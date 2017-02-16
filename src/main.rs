abcd779ae0008091f00ac97fb56ccb00d236f2fa
Szymon Urbaś <szymon.urbas@aol.com>
2017-02-16 19:17:25 +0100

    Slightly better exception system

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
 #![feature(lang_items)]
+#![feature(asm)]
 #![no_main]
 #![no_std]
 
 #[export_name = "_reset"]
 pub extern "C" fn main() -> ! {
-  let y;
-  let x = 42;
-
-  y = x;
+  unsafe {
+    let sram_boundary = *(0x0000_0000 as *const u32);
+    let crash = *(sram_boundary as *const u32);
+  }
 
   loop {}
 }
 
-#[export_name = "_nmi_isr"]
-pub extern "C" fn nmi_isr() -> ! {
-  loop {}
-}
+mod exception {
+  pub extern "C" fn dummy_handler() {
+    unsafe { asm!("bkpt"); }
+    loop {}
+  }
 
-#[export_name = "_hard_fault_isr"]
-pub extern "C" fn hard_fault_isr() -> ! {
-  loop {}
+  #[export_name = "_EXCEPTIONS"]
+  pub static EXCEPTIONS: [Option<extern "C" fn()>; 14] = [
+    Some(dummy_handler), // NMI
+    Some(dummy_handler), // Hard fault
+    Some(dummy_handler), // Memmanage fault
+    Some(dummy_handler), // Bus fault
+    Some(dummy_handler), // Usage fault
+    None, // Reserved
+    None, // Reserved
+    None, // Reserved
+    None, // Reserved
+    Some(dummy_handler), // SVC call
+    None, // Reserved for debug
+    None, // Reserved
+    Some(dummy_handler), // PendSV
+    Some(dummy_handler), // Systick
+  ];
 }
 
 mod lang_items {
